4190e47099f61fe6a08bd797b4db53a0de3fdaee
radhikagoyal <12rgoyal@gmail.com>
2015-02-28 12:13:41 -0600

    Create exif_parser.h

---
+++ exif_parser.h
@@ -0,0 +1,19 @@
+/**
+ * @file exif_parser.h
+ * Definition of a ExifParser class.
+ *
+ * @author Radhika Goyal, Max Zhang
+ * @date Spring 2011
+ * @date Summer 2012
+ */
+
+#ifndef _EXIF_PARSE_H_
+#define _EXIF_PARSE_H_
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+
+
